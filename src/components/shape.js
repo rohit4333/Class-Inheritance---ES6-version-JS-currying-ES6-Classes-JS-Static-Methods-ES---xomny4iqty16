e3ec4b23b8755c0afa99a2f4d714c566aa70a7e5
Rohit Vishwakarma <Rohit993583@gmail.com>
2022-10-25 12:25:59 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,20 +1,51 @@
+// // declare class
+
+// // export class using module.exports
+
+// class Shape{
+//     constructor(){
+//         this.color = "red";
+//     }
+
+//     drawShape(){
+//         console.log("shape drawn");
+//     }
+//     calculateArea(){
+//         console.log("Area calucted")
+//     }
+// }
+
+// module.exports = {
+//     Shape : Shape
+// }
+
 // declare class
+
+let Shape = class {
+
+    drawShape() {
+
+        console.log("Drawing Shape");
+
+    }
+
+    calculateArea() {
+
+        console.log("calculating Area");
+
+    }
+
+    color(rang) {
+
+        console.log(`filling in the ${rang}`);
+
+    }
+
+}
+
+
+ 
 
 // export class using module.exports
 
-class Shape{
-    constructor(){
-        this.color = "red";
-    }
-
-    drawShape(){
-        console.log("shape drawn");
-    }
-    calculateArea(){
-        console.log("Area calucted")
-    }
-}
-
-module.exports = {
-    Shape : Shape
-}
+module.exports = Shape;
