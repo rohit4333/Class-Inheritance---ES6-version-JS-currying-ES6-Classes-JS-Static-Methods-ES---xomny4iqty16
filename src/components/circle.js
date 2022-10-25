ade928a74fee76a313ab4dd5871bb2a77aaf6928
Rohit Vishwakarma <Rohit993583@gmail.com>
2022-10-25 12:25:11 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,22 +1,46 @@
-// import using require
+// // import using require
+
+// // declare class
+
+
+// // export class using module.exports
+
+// let Shape = require("./shape");
+
+// class Circle extends Shape{
+//     constructor(){
+//         super();
+//     }
+//     calculateArea(){
+//         console.log("first")
+//     }
+// }
+
+
+// module.exports = {
+//     Circle : Circle
+// }
+const Shape = require('./shape');
+
+
+ 
 
 // declare class
 
+let Circle = class extends Shape {
+
+    calculateArea() {
+
+        console.log("Area Calculating for Circle");
+
+    }
+
+}
+
+
+
+ 
 
 // export class using module.exports
 
-let Shape = require("./shape");
-
-class Circle extends Shape{
-    constructor(){
-        super();
-    }
-    calculateArea(){
-        console.log("first")
-    }
-}
-
-
-module.exports = {
-    Circle : Circle
-}
+module.exports = Circle;
